8651f234a6a4bb09d92cb94e78eed1d24009fbf7
Justin Niles <justinni@microsoft.com>
2020-10-23 21:53:19 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import Sortable from 'sortablejs';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+let initialItems = [];
+for (let i = 0; i < 5; i++) {
+  initialItems.push(`Item ${i}`);
+}
 
 function App() {
+  const listRef = useRef(null);
+  const [items, setItems] = useState(initialItems);
+
+  const itemsRef = useRef({
+    items,
+    setItems,
+  });
+  useEffect(() => {
+    itemsRef.current = {
+      items,
+      setItems,
+    };
+  }, [items, setItems]);
+
+  useEffect(() => {
+    const listRefCurrent = listRef.current;
+    if (listRefCurrent) {
+      Sortable.create(listRefCurrent, {
+        animation: 150,
+        onEnd: ({ oldIndex, newIndex }) => {
+          console.log(`Old index: ${oldIndex}, New index: ${newIndex}`);
+
+          if (oldIndex !== newIndex) {
+            // Update items order
+            const { items, setItems } = itemsRef.current;
+            const movedItem = items[oldIndex];
+            if (!movedItem) {
+              console.warn("Can't find moved item at index ", oldIndex);
+              return;
+            }
+
+            const itemToSwapWith = items[newIndex];
+            if (!itemToSwapWith) {
+              console.warn('Ended on out of bounds index: ', newIndex);
+              return;
+            }
+
+            const newItems = [...items];
+            newItems[oldIndex] = itemToSwapWith;
+            newItems[newIndex] = movedItem;
+
+            setItems(newItems);
+          }
+        },
+      });
+    }
+  });
+
+  const onAddItem = useCallback(() => {
+    setItems([...items, `Item ${items.length}`]);
+  }, [items, setItems]);
+
+  console.log(items);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol className="list" ref={listRef}>
+        {items.map((item) => (
+          <li className="list-item" key={item}>
+            <div className="item">{item}</div>
+          </li>
+        ))}
+      </ol>
+      <div className="footer">
+        <button onClick={onAddItem}>Add item</button>
+      </div>
     </div>
   );
 }
